74242c20662fa4ce7ac029e02c8b321cf0e1cd4b
Nash Gichohi <166515383+Kich254@users.noreply.github.com>
2025-09-15 18:56:57 +0300

    edits

---
+++ database/_create_insert_ace_and_partner_r-ship.sql
@@ -0,0 +1,10 @@
+-- ===============================================================================
+-- POSTGRESQL ACE AND PARTNER RELATIONSHIPS SCHEMA CREATION 
+-- ===============================================================================
+CREATE SCHEMA IF NOT EXISTS aces_and_partner_rships;
+SET search_path TO aces_and_partner_rships, public;
+
+-- ============================================================================
+-- Table creation, data insert and queries 
+-- Source: ""; Table .
+-------------------------------------------------------------------------------
