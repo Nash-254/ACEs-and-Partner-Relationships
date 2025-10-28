69998cccfcec1f1b0890404e60420be00f6b5a8b
Nash Gichohi <166515383+Kich254@users.noreply.github.com>
2025-10-28 10:10:14 +0300

    updates

--- database/_create_insert_ace_and_partner_r-ships_schema.sql
+++ database/_create_insert_ace_and_partner_r-ships_schema.sql
@@ -5,7 +5,7 @@ CREATE SCHEMA IF NOT EXISTS aces_and_partner_rships;
 SET search_path TO aces_and_partner_rships, public;
 
 -- ============================================================================
--- Table creation, data insert and queries for Odds Ratios of Partner-Reported ACE Categories One Year Postpartum When Mothers Had the Same Experience.
+--1. Table creation, data insert and queries for Odds Ratios of Partner-Reported ACE Categories One Year Postpartum When Mothers Had the Same Experience.
 -- Source: "Adverse Childhood Experiences are associated with choice of partner, both partners' relationship and psychosocial health as reported one year after birth of a common child. A cross-sectional study"; Table 3.
 -------------------------------------------------------------------------------
 
@@ -50,3 +50,36 @@ ORDER BY adjusted_odds_ratio DESC;
 -- END of Table creation, data insert and queries for Odds Ratios of Partner-Reported ACE Categories One Year Postpartum When Mothers Had the Same Experience.
 -- ===============================================================================
 
+
+
+-- =========================================================
+--2. Table creation, data insert and queries for Proportions of outcome measures related to groups of couples with different numbers of ACE category exposure.
+-- Source: "Adverse Childhood Experiences are associated with choice of partner, both partners' relationship and psychosocial health as reported one year after birth of a common child. A cross-sectional study"; Table 4.
+-- =========================================================
+
+-- TABLE CREATION
+CREATE TABLE aces_and_partner_rships.relationship_outcomes (
+    ace_group VARCHAR(20) PRIMARY KEY,
+    low_education NUMERIC(5,1),
+    low_income NUMERIC(5,1),
+    bad_spousal_support NUMERIC(5,1),
+    rshp_assess_score NUMERIC(5,1),
+    bad_health NUMERIC(5,1),
+    stress NUMERIC(5,1),
+    anxiety NUMERIC(5,1),
+    depression NUMERIC(5,1),
+    p_value NUMERIC(5,3)    
+);
+
+-- DATA INSERTION
+INSERT INTO aces_and_partner_rships.relationship_outcomes
+(ace_group, low_education, low_income, bad_spousal_support, rshp_assess_score, bad_health, stress, anxiety, depression, p_value)
+VALUES
+('none', 19.0, 6.4, 5.1, 9.6, 5.2, 5.8, 0, 0),
+('0-3_both', 24.4, 10.6, 5.6, 9.8, 10.8, 8.0, 0.6, 0.6),
+('mother_high', 28.2, 16.2, 9.5, 16.4, 14.9, 20.4, 2.7, 2.7),
+('partner_high', 20.4, 16.4, 9.1, 18.2, 21.8, 20.4, 5.4, 1.8),
+('both_high', 66.7, 33.3, 33.3, 26.7, 60.0, 20.0, 13.3);
+
+-- VERIFY DATA INTEGRITY
+SELECT * FROM relationship_outcomes;
